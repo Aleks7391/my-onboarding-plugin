1ace77185e1f51a51e65cadea6ce8e244e87f362
Aleks7391 <aleks.ganev7@gmail.com>
2021-05-19 18:52:50 +0300

    Added My Onboard Settings page

--- my-onboarding-plugin.php
+++ my-onboarding-plugin.php
@@ -39,3 +39,37 @@ add_filter( 'the_content', 'ag_add_hidden_div', 10);
 add_filter( 'wp_nav_menu_items', 'add_profile_settings_link', 11);
 add_action( 'profile_update', 'send_email_on_profile_edit');
 
+function my_plugin_menu() {
+	add_options_page( 'My Onboarding Settings', 'My Onboarding', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
+}
+
+add_action( 'admin_menu', 'my_plugin_menu' );
+
+function my_plugin_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	//Page Title
+	echo '<div class="wrap">
+			<h1>My Onboarding Settings</h1>';
+	//Filters Enabled checkbox field
+	echo '<form method="post" action="options.php" style="clear: both">
+			<table class="form-table">
+			<tbody>
+			<tr>
+			<td>
+			<fieldset>
+			<input type="checkbox" name="filters-enabled" id="filters-enabled" value="1"></input>
+			<label for="filters-enabled">Filters Enabled</label>
+			</fieldset>
+			</td>
+			</tr>
+			</tbody>
+			</table>
+			</form>';
+	//Save changes button
+	echo '<p class="submit">
+			<input type="submit" class="button button-primary" value="Save changes">
+			</p>
+			</div>';
+}
