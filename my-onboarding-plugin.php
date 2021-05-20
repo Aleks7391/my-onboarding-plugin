8e5a86ad3faf8eb2fe7b677caafdb7f3f8b19e38
Aleks7391 <aleks.ganev7@gmail.com>
2021-05-20 14:27:50 +0300

    Added checkbox in options to toggle the filter on or off

--- my-onboarding-plugin.php
+++ my-onboarding-plugin.php
@@ -32,44 +32,62 @@ function send_email_on_profile_edit() {
 	wp_mail( $admin_email, 'Profile Update', 'Your profile has just been updated' );
 }
 
-add_filter( 'the_content', 'ag_change_content', 7);
-add_filter( 'the_content', 'ag_add_hidden_div', 8);
-add_filter( 'the_content', 'ag_add_paragraph', 9);
-add_filter( 'the_content', 'ag_add_hidden_div', 10);
-add_filter( 'wp_nav_menu_items', 'add_profile_settings_link', 11);
-add_action( 'profile_update', 'send_email_on_profile_edit');
+add_action( 'admin_init', 'ag_initialize_my_onboarding_options', 6 );
+add_action( 'admin_menu', 'ag_create_my_onboarding_options_page', 6 );
 
-function my_plugin_menu() {
-	add_options_page( 'My Onboarding Settings', 'My Onboarding', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
+add_action( 'init', 'ag_apply_filter' );
+
+function ag_apply_filter() {
+	$option = get_option( 'toggle_filter' );
+	if ( $option === '1' ) {
+		add_filter( 'the_content', 'ag_change_content', 7 );
+		add_filter( 'the_content', 'ag_add_hidden_div', 8 );
+		add_filter( 'the_content', 'ag_add_paragraph', 9 );
+		add_filter( 'the_content', 'ag_add_hidden_div', 10 );
+		add_filter( 'wp_nav_menu_items', 'add_profile_settings_link', 11 );
+		add_action( 'profile_update', 'send_email_on_profile_edit' );		
+	}
 }
 
-add_action( 'admin_menu', 'my_plugin_menu' );
+function ag_create_my_onboarding_options_page() {
+    add_options_page(
+        'My Onboarding Options',
+        'My Onboarding',
+        'manage_options',
+        'onboarding',
+        'ag_initialize_my_onboarding_options'
+    );
+}
 
-function my_plugin_options() {
-	if ( !current_user_can( 'manage_options' ) )  {
-		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
-	}
-	//Page Title
-	echo '<div class="wrap">
-			<h1>My Onboarding Settings</h1>';
-	//Filters Enabled checkbox field
-	echo '<form method="post" action="options.php" style="clear: both">
-			<table class="form-table">
-			<tbody>
-			<tr>
-			<td>
-			<fieldset>
-			<input type="checkbox" name="filters-enabled" id="filters-enabled" value="1"></input>
-			<label for="filters-enabled">Filters Enabled</label>
-			</fieldset>
-			</td>
-			</tr>
-			</tbody>
-			</table>
-			</form>';
-	//Save changes button
-	echo '<p class="submit">
-			<input type="submit" class="button button-primary" value="Save changes">
-			</p>
-			</div>';
+function ag_initialize_my_onboarding_options() {
+    add_settings_section(
+        'ag-my-onboarding-section',          // ID
+        'My Onboarding',                     // Title
+        'ag_my_onboarding_options_callback', // Callback
+        'general'                            // Page
+    );
+     
+    add_settings_field( 
+        'toggle_filter',                     // ID
+        'Filter',                            // Label
+        'ag_toggle_filter_callback',         // Callback
+        'general',                           // Page
+        'ag-my-onboarding-section'           // Section
+    );
+
+    register_setting(
+        'general',
+        'toggle_filter'
+    );
+}
+
+function ag_my_onboarding_options_callback() {
+    //
+}
+
+function ag_toggle_filter_callback() {
+    $html = '<input type="checkbox" id="toggle_filter" name="toggle_filter" value="1" ' . checked(1, get_option('toggle_filter'), false) . '/>'; 
+    $html .= '<label for="toggle_filter"> Activate this setting to toggle the filter.</label>'; 
+     
+    echo $html;
 }
