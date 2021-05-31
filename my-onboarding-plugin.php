bd0e602318e8f37d3983cab1c25b2e8984cfb901
Aleks7391 <aleks.ganev7@gmail.com>
2021-05-31 22:35:48 +0300

    Replaced some spaces with tabs. Renamed some functions

--- my-onboarding-plugin.php
+++ my-onboarding-plugin.php
@@ -20,13 +20,13 @@ function ag_add_paragraph( $content ) {
 	return preg_replace( '<p>', 'p>This Paragraph was added programmatically.</p><p', $content, 1 );
 }
 
-function add_profile_settings_link( $items ) {
+function ag_add_profile_settings_link( $items ) {
 	if ( is_user_logged_in() ) {
-		return $items . '<li><a href="'. site_url('wp-admin/profile.php') .'">Profile Settings</a></li>';
+		return $items . '<li><a href="' . site_url( 'wp-admin/profile.php' ) . '">Profile Settings</a></li>';
 	}
 }
 
-function send_email_on_profile_edit() {
+function ag_send_email_on_profile_edit() {
 	$admin_email = get_option( 'admin_email' );
 	wp_mail( $admin_email, 'Profile Update', 'Your profile has just been updated' );
 }
@@ -39,23 +39,24 @@ add_action( 'template_redirect', 'ag_apply_filter' );
 function ag_apply_filter() {
 	$option = get_option( 'toggle_filter' );
 	if  ( is_singular( 'student' ) && '1' === $option ) {
-		add_filter( 'the_content', 'ag_add_hidden_div' );
-		add_filter( 'the_content', 'ag_add_paragraph' );
 		add_filter( 'the_content', 'ag_add_hidden_div', 11 );
-		add_filter( 'the_content', 'ag_change_content', 12 );
-		add_filter( 'wp_nav_menu_items', 'add_profile_settings_link' );
-		add_action( 'profile_update', 'send_email_on_profile_edit' );	
+		add_filter( 'the_content', 'ag_add_paragraph', 12 );
+		add_filter( 'the_content', 'ag_add_hidden_div', 13 );
+		add_filter( 'the_content', 'ag_change_content', 14 );
+		add_filter( 'wp_nav_menu_items', 'ag_add_profile_settings_link' );
+		add_action( 'profile_update', 'ag_send_email_on_profile_edit' );	
 	}
 }
 
+//Toggle filter settings page
 function ag_create_my_onboarding_options_page() {
-    add_menu_page(
-        'My Onboarding Options',
-        'My Onboarding',
-        'manage_options',
-        'onboarding',
-        'ag_render_onboard_settings_page'
-    );
+	add_menu_page(
+		'My Onboarding Options',
+		'My Onboarding',
+		'manage_options',
+		'onboarding',
+		'ag_render_onboard_settings_page'
+	);
 }
 
 function ag_render_onboard_settings_page() {
@@ -78,32 +79,30 @@ function ag_initialize_my_onboarding_options() {
 		'toggle_filter'
 	);
 
-    add_settings_section(
-        'ag-my-onboarding-section',          // ID
-        'My Onboarding',                     // Title
-        'ag_my_onboarding_options_callback', // Callback
-        'onboarding'                         // Page
-    );
-     
-    add_settings_field( 
-        'toggle_filter',                     // ID
-        'Filter',                            // Label
-        'ag_toggle_filter_callback',         // Callback
-        'onboarding',                        // Page
-        'ag-my-onboarding-section'           // Section
-    );
+	add_settings_section(
+		'ag-my-onboarding-section',          // ID.
+		'My Onboarding',                     // Title.
+		'ag_my_onboarding_options_callback', // Callback.
+		'onboarding'                         // Page.
+	);
+
+	add_settings_field(
+		'toggle_filter',					 // ID.
+		'Filter',							// Label.
+		'ag_toggle_filter_callback',		 // Callback.
+		'onboarding',						// Page.
+		'ag-my-onboarding-section'		   // Section.
+	);
 }
 
 function ag_my_onboarding_options_callback() {
-    //
 }
 
 function ag_toggle_filter_callback() {
-	if (!current_user_can('manage_options'))  {
-		wp_die( __('You do not have sufficient permissions to access this page.') );
+	if ( ! current_user_can('manage_options' ) ) {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
-    $html = '<input type="checkbox" id="toggle_filter" name="toggle_filter" value="1" ' . checked(1, get_option( 'toggle_filter' ), false) . '/>'; 
-    $html .= '<label for="toggle_filter"> Activate this setting to toggle the filter.</label>'; 
-     
-    echo $html;
+	$html  = '<input type="checkbox" id="toggle_filter" name="toggle_filter" value="1" ' . checked( 1, get_option( 'toggle_filter' ), false ) . '/>';
+	$html .= '<label for="toggle_filter"> Activate this setting to toggle the filter.</label>';
+	echo $html;
 }
