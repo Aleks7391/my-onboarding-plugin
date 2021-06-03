b6964232ba5ed7938b700a61d209f830b9cb0736
Aleks7391 <aleks.ganev7@gmail.com>
2021-06-03 11:41:53 +0300

    Added comments to functions

--- my-onboarding-plugin.php
+++ my-onboarding-plugin.php
@@ -8,24 +8,32 @@
 
 function ag_change_content( $content ) {
 	$prepend = '<p>Onboarding Filter: </p>';
-	$append = '<p>by Aleks Ganev</p>';
+	$append  = '<p>by Aleks Ganev</p>';
 	return $prepend . $content . $append;
 }
 
+/**
+ * Adds a hidden div element after the first </p> tag	
+ * 
+ * @params stts
+ */
 function ag_add_hidden_div( $content ) {
 	return preg_replace( '</p>', '/p><div style="display:none">Hello</div', $content, 1 );
 }
 
+//Adds a paragraph element to the content
 function ag_add_paragraph( $content ) {
 	return preg_replace( '<p>', 'p>This Paragraph was added programmatically.</p><p', $content, 1 );
 }
 
+//Adds a link to the user's profile page to nav menu
 function ag_add_profile_settings_link( $items ) {
 	if ( is_user_logged_in() ) {
 		return $items . '<li><a href="' . site_url( 'wp-admin/profile.php' ) . '">Profile Settings</a></li>';
 	}
 }
 
+//Sends an email to the user upon editing his profile
 function ag_send_email_on_profile_edit() {
 	$admin_email = get_option( 'admin_email' );
 	wp_mail( $admin_email, 'Profile Update', 'Your profile has just been updated' );
@@ -36,19 +44,20 @@ add_action( 'admin_init', 'ag_initialize_my_onboarding_options', 6 );
 
 add_action( 'template_redirect', 'ag_apply_filter' );
 
+//Applies the filter if the according to the settings
 function ag_apply_filter() {
 	$option = get_option( 'toggle_filter' );
-	if  ( is_singular( 'student' ) && '1' === $option ) {
+	if ( is_singular( 'student' ) && '1' === $option ) {
 		add_filter( 'the_content', 'ag_add_hidden_div', 11 );
 		add_filter( 'the_content', 'ag_add_paragraph', 12 );
 		add_filter( 'the_content', 'ag_add_hidden_div', 13 );
 		add_filter( 'the_content', 'ag_change_content', 14 );
 		add_filter( 'wp_nav_menu_items', 'ag_add_profile_settings_link' );
-		add_action( 'profile_update', 'ag_send_email_on_profile_edit' );	
+		add_action( 'profile_update', 'ag_send_email_on_profile_edit' );
 	}
 }
 
-//Toggle filter settings page
+//Creates the Onboarding Filter settings page
 function ag_create_my_onboarding_options_page() {
 	add_menu_page(
 		'My Onboarding Options',
@@ -59,6 +68,7 @@ function ag_create_my_onboarding_options_page() {
 	);
 }
 
+//Callback for the settings page
 function ag_render_onboard_settings_page() {
 	?>
 	<div class="wrap">
@@ -73,6 +83,7 @@ function ag_render_onboard_settings_page() {
 	<?php
 }
 
+//Initialize the options
 function ag_initialize_my_onboarding_options() {
 	register_setting(
 		'onboarding',
@@ -88,18 +99,20 @@ function ag_initialize_my_onboarding_options() {
 
 	add_settings_field(
 		'toggle_filter',					 // ID.
-		'Filter',							// Label.
+		'Filter',							 // Label.
 		'ag_toggle_filter_callback',		 // Callback.
-		'onboarding',						// Page.
-		'ag-my-onboarding-section'		   // Section.
+		'onboarding',						 // Page.
+		'ag-my-onboarding-section'		     // Section.
 	);
 }
+
 
 function ag_my_onboarding_options_callback() {
 }
 
+//Callback for the toggle filter option
 function ag_toggle_filter_callback() {
-	if ( ! current_user_can('manage_options' ) ) {
+	if ( ! current_user_can( 'manage_options' ) ) {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
 	$html  = '<input type="checkbox" id="toggle_filter" name="toggle_filter" value="1" ' . checked( 1, get_option( 'toggle_filter' ), false ) . '/>';
